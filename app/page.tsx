8012b50b848c6b285f6b1ede9a393832b23db55a
resomee <resomee@nate.com>
2023-12-04 22:43:08 +0900

    fix home link

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,10 @@ import VideoDetail from "@/components/VideoDetail";
 export default function Home() {
   return (
     <>
-      <header className=" bg-slate-900 text-white p-4">
-        <h1 className="font-bold text-xl">Youtube Downloader</h1>
+      <header className=" bg-slate-900 text-white flex">
+        <a className="w-56 flex items-center justify-center m-2 p-2" href="/">
+          <h1 className="font-bold text-xl">Youtube Downloader</h1>
+        </a>
       </header>
       <main>
         <SubmitForm />
